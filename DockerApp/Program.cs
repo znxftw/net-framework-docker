a01229d4388423ede577097981acd9599528b04c
znxftw <vishnu2101@gmail.com>
2024-09-04 11:28:08 +0530

    initial commit

---
+++ DockerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DockerApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+          Console.WriteLine("Hello World!");
+        }
+    }
+}
